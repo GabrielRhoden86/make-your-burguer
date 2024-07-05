2e7ca84e5dbccb65ed705d28c3898b44a65a2c10
GabrielRhoden86 <gabrielrhodden@gmail.com>
2024-07-05 00:21:13 -0300

    correção view home

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
 
 const routes = [
   {
@@ -10,7 +10,7 @@ const routes = [
   {
     path: '/pedidos',
     name: 'pedidos',
-    component: () => import(/* webpackChunkName: "pedidos" */ '../views/Pedidos.vue')
+    component: () => import('../views/Pedidos.vue')
   }
 ]
 
