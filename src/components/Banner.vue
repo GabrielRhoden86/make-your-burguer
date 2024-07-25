28bf63a4e7be0bf28cd72b1c3912da7881dd66ee
GabrielRhoden86 <gabrielrhodden@gmail.com>
2024-07-24 23:36:05 -0300

    correção items form

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,54 +5,8 @@
 </template>
 
 <script>
+import "@/assets/css/banner.css"; 
 export default {
   name:'Banner'
 }
 </script>
-
-<style>
-#main-banner{              
- background-image: url('/public/img/burger.jpg');
- background-position: 0 -250px;
- background-repeat: no-repeat;
- background-size: cover; 
- height:700px;
- display:flex;
- align-items: center;
- justify-content: flex-start;
-}
-
-/* Tablet (a partir de 768px de largura) */
-@media (max-width: 1024px) {
-  #main-banner {
-    height: 450px; /* 700px - 150px */
-  }
-
-}
-
-/* Celular (a partir de 768px de largura) */
-@media (max-width: 768px) {
-  #main-banner {
-    height: 300px; /* 550px - 150px */
-  }
-}
-
-/* Dispositivos menores (opcional) */
-@media (max-width: 576px) {
-  #main-banner {
-    height: 300px; /* 400px - 150px */
-  }
-  h1{
-    font-size:3.5rem !important;
-  }
-}
-
-#main-banner h1{
-    color:#fafafa;
-    text-align: center;
-    font-size:4em;
-    background-color:#222;
-    padding:20px 40px;
-  
-}
-</style>
