efbe6fb09358b7b794f525eda6bde148ddc17c06
Gabriel Rhoden <gabrielrhodden@gmail.com>
2024-08-08 17:12:56 -0300

    finalizado post formulário

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,44 +1,41 @@
-<template>
-    <div>
-        <p>Componente de Menssagem</p>
-    </div>
-  <div class="d-flex justify-content-center">
-     <form class="col-md-5" id="form-burguer" action="" >
-      <div class="mb-3">
-        <label for="nome" class="form-label">Nome do cliente:</label>
-        <input type="text" class="form-control" id="nome" v-model="nome">
-      </div>
-      <div class="mb-3">
-        <label for="pao" class="form-label">Escolha o pão:</label>
-        <select class="form-select" id="pao" >
-          <option>Selecione</option>
-          <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
-        </select>
+ <template>
+   <div>
+     <p>Componente de Menssagem</p>
+   </div>
+   <div class="d-flex justify-content-center">
+   <form class="col-md-5" id="form-burguer" @submit="createBurguer" >
+     <div class="mb-3">
+       <label for="nome" class="form-label">Nome do cliente:</label>
+       <input type="text" class="form-control" id="nome" v-model="nome">
+     </div>
+     <div class="mb-3">
+       <label for="pao" class="form-label">Escolha o pão:</label>
+       <select class="form-select" id="pao" v-model="pao">
+         <option>Selecione</option>
+         <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
+       </select>
       </div>
       <div class="mb-3">
         <label for="carne" class="form-label">Escolha a carne do seu Burguer:</label>
         <select class="form-select" id="carne" v-model="carne">
           <option>Selecione</option>
-
           <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
       </select>
-   </div>
- <div class="mb-3 r">
-  <label for="opcionais" class="form-label">Selecione os opcionais:</label>
-
-  <div class="form-check-container">
-  <div class="form-check" v-for="opcional in opcionaisdata" :key="opcional.id">
-    <input type="checkbox" class="form-check-input" :id="`salame${opcional.id}`" v-model="opcional.tipo" :value="opcional.tipo">
-    <label class="form-check-label">{{ opcional.tipo }}</label>
+    </div>
+   <div class="mb-3 r">
+    <label for="opcionais" class="form-label">Selecione os opcionais:</label>
+    <div class="form-check-container">
+    <div class="form-check" v-for="opcional in opcionaisdata" :key="opcional.id">
+      <input type="checkbox" class="form-check-input" :id="`salame${opcional.id}`" :value="opcional.tipo" v-model="opcionais">
+      <label class="form-check-label">{{ opcional.tipo }}</label>
+    </div>
+    </div>
   </div>
-</div>
-
- </div>
-  <div class="mb-3">
+   <div class="mb-3">
     <button type="submit" class="btn btn-form w-100" >Criar meu burguer</button>
-  </div>
- </form>
-</div>
+   </div>
+  </form>
+ </div>
 </template>
 
 <script>
@@ -65,7 +62,28 @@ export default {
       this.paes = data.paes;
       this.carnes = data.carnes;
       this.opcionaisdata = data.opcionais;
-      console.log(data);
+    },
+    async createBurguer(e){
+      e.preventDefault();
+      const data = {
+        nome:this.nome,
+        carne:this.carne,
+        pao:this.pao,
+        opcionais:Array.from(this.opcionais),
+        status:"Solcitado"
+      }
+      const JasonData = JSON.stringify(data);
+      const request = await fetch('http://localhost:3000/burgers',{ 
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JasonData
+      });
+      const response = await request.json();
+      console.log(response);
+      this.nome = "",
+      this.carne = "",
+      this.pao = "",
+      this.opcionais = ""
     }
   },
   mounted(){
